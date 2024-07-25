71985e056163ca6f4ff7d7148913878a4b038f73
jaakka <jaska.talvitie@gmail.com>
2024-07-25 19:35:00 +0300

    some updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CanbusHandler',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'python-can',
